b9de497208774822c9a339dfa98c0b9a1e5c3a00
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2021-12-19 13:52:23 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyB1uZKMv8SpQh99hoEPHrrUcA3kmUZ8egM',
+  authDomain: 'chat-web-app-94cab.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-94cab-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'chat-web-app-94cab',
+  storageBucket: 'chat-web-app-94cab.appspot.com',
+  messagingSenderId: '599230125597',
+  appId: '1:599230125597:web:81ff290aaa3c4cb94098f4',
+};
+
+const app = firebase.initializeApp(config);
